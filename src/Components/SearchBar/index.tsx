bc356162d35c3f81b8065df72fad94cfdd7ca6e0
Luiz F Dias <luizfd@gmail.com>
2023-09-17 15:25:09 -0300

    SearchBar Component update

--- src/Components/SearchBar/index.tsx
+++ src/Components/SearchBar/index.tsx
@@ -3,9 +3,9 @@ import "./styles.css";
 export default function SearchBar() {
   return (
     <>
-      <form action="">
-        <input type="text" placeholder="Digite sua busca"></input>
-        <button type="submit">Buscar</button>
+      <form action="" className="ct-search-bar">
+        <input className="ct-form-control" type="text" placeholder="Digite sua busca"></input>
+        <button className="ct-search-bar-submit" type="submit">Buscar</button>
       </form>
     </>
   );
